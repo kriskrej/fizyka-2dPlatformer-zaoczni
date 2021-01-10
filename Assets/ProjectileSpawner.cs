df203ddcd0922abffd44569d4c54536067da83ab
kris <kris@krej.net>
2021-01-10 09:33:00 +0100

    projectile spawner

---
+++ Assets/ProjectileSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour {
+    [Range(0.1f, 1f)] public float spawnTimeInterval = 0.3f;
+    [SerializeField] GameObject projectilePrefab;
+    void Start() {
+        InvokeRepeating("Spawn", spawnTimeInterval, spawnTimeInterval);
+    }
+
+    void Spawn() {
+        Instantiate(projectilePrefab);
+    }
+
+}
